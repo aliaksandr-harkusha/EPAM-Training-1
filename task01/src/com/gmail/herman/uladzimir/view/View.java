427e067fbcf12231a7ed6729422d6db63cdcb3ba
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 15:46:52 +0300

    Update View.java

--- task01/src/com/gmail/herman/uladzimir/view/View.java
+++ task01/src/com/gmail/herman/uladzimir/view/View.java
@@ -1,3 +1,5 @@
+//класс по сути тоже избыточен
+//лучше просто вызывай System.out.println - в main методе когда нужно что-то вывести пользователю
 package com.gmail.herman.uladzimir.view;
 
 public class View {
