816609a658d87aa8744525fe429e8966207b56a6
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 15:41:59 +0300

    Update TariffSorter.java

--- task01/src/com/gmail/herman/uladzimir/model/functional/TariffSorter.java
+++ task01/src/com/gmail/herman/uladzimir/model/functional/TariffSorter.java
@@ -5,11 +5,14 @@ import com.gmail.herman.uladzimir.model.entity.Tariff;
 public class TariffSorter<T extends Comparable<T>> {
 
     public void sortBySubscriptionFee() {
+        //пока синглтоны не будем использовать
+        //лучше создавай объекты пока по старинке: TariffWorker tariffWorker = new TariffWorker(); 
         Tariff tariffsArray[] = TariffWorker.getInstance().getAllTariff();
         Tariff tempTariff;
         int iteration = tariffsArray.length - 1;
         boolean sort;
-
+        //для сортировки в java используется метод: Collection.sort();
+        //свою сортировку не нужно изобретать
         for (int j = iteration; j > 0; j--) {
             sort = false;
 
